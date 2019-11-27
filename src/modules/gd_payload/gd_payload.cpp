98f7afeffbf426c5385788b224a9a28cf14e1585
Maximilian Laiacker <m.laiacker@germandrones.com>
2019-11-27 16:52:30 +0100

    hil fixed

--- src/modules/gd_payload/gd_payload.cpp
+++ src/modules/gd_payload/gd_payload.cpp
@@ -620,7 +620,7 @@ bool  GDPayload::readPayloadAdc()
 				sim_current_a += 5.0f + 180.0f*ctrl.control[actuator_controls_s::INDEX_THROTTLE]*ctrl.control[actuator_controls_s::INDEX_THROTTLE];
 			} else{
 				sim_current_a += 1.0f + 75.0f*ctrl.control[actuator_controls_s::INDEX_THROTTLE]*ctrl.control[actuator_controls_s::INDEX_THROTTLE]
-							     + rand()*10f/RAND_MAX;
+							     + rand()*10.0f/RAND_MAX;
 			}
 			if(_batt_sim.connected)
 			{
