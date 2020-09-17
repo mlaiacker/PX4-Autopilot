37f1ba41e04b20e5049a464ffcd2324605ff4138
Maximilian Laiacker <m.laiacker@germandrones.com>
2020-09-17 16:13:14 +0200

    using param3 of reposition to change loiter radius during loiter

--- src/modules/navigator/navigator_main.cpp
+++ src/modules/navigator/navigator_main.cpp
@@ -269,6 +269,14 @@ Navigator::run()
 
 					rep->current.loiter_radius = get_loiter_radius();
 					rep->current.loiter_direction = 1;
+					/* ML: use param 3 as loiter radius and check vor valid radius valid loiter radius */
+					if(fabsf(cmd.param3)>10.0f && PX4_ISFINITE(cmd.param3) && fabsf(cmd.param3)<1000.0f)
+					{
+						rep->current.loiter_radius = fabsf(cmd.param3);
+						if(cmd.param3<0.0f)	{
+							rep->current.loiter_direction = -1;
+						}
+					}
 					rep->current.type = position_setpoint_s::SETPOINT_TYPE_LOITER;
 
 					// If no argument for ground speed, use default value.
