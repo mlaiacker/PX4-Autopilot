5baaa2b165daa64bc0bf8a18af05731e55d36600
Maximilian Laiacker <m.laiacker@germandrones.com>
2021-09-17 16:34:58 +0200

    songbird makefile

--- boards/px4/fmu-v3/songbird.cmake
+++ boards/px4/fmu-v3/songbird.cmake
@@ -1,6 +1,3 @@
-
-# FMUv3 is FMUv2 with access to the full 2MB flash
-
 px4_add_board(
 	PLATFORM nuttx
 	VENDOR px4
@@ -8,64 +5,57 @@ px4_add_board(
 	LABEL songbird
 	TOOLCHAIN arm-none-eabi
 	ARCHITECTURE cortex-m4
+	CONSTRAINED_MEMORY
 	ROMFSROOT px4fmu_common
 	IO px4_io-v2_default
-	TESTING
 	UAVCAN_INTERFACES 2
 	SERIAL_PORTS
 		GPS1:/dev/ttyS3
 		TEL1:/dev/ttyS1
 		TEL2:/dev/ttyS2
-		GPS2:/dev/ttyS6
+		TEL4:/dev/ttyS6
 	DRIVERS
-		adc
+		adc/ads1115
+		adc/board_adc
 		barometer # all available barometer drivers
-		#batt_smbus
+		batt_smbus
 		camera_capture
 		camera_trigger
 		differential_pressure # all available differential pressure drivers
 		distance_sensor # all available distance sensor drivers
-		#dshot
+		dshot
 		gps
-		#heater
+		heater
 		#imu # all available imu drivers
-		imu/adis16448
-		imu/adis16477
-		imu/adis16497
+		imu/analog_devices/adis16448
 		imu/l3gd20
 		imu/lsm303d
 		imu/invensense/icm20608g
-		imu/invensense/icm20948
+		imu/invensense/icm20948 # required for ak09916 mag
 		imu/invensense/mpu6000
 		imu/invensense/mpu9250
 		irlock
-		lights/blinkm
-		lights/rgbled
-		lights/rgbled_ncp5623c
+		lights # all available light drivers
 		magnetometer # all available magnetometer drivers
-		mkblctrl
 		optical_flow # all available optical flow drivers
-		#optical_flow/px4flow
-		#osd
-		#pca9685
-		power_monitor/ina226
+		osd
+		pca9685
+		pca9685_pwm_out
+		#power_monitor/ina226
 		#protocol_splitter
 		pwm_input
 		pwm_out_sim
 		pwm_out
 		px4io
-		#roboclaw
-		#tap_esc
+		roboclaw
+		rpm
+		smart_battery/batmon
 		telemetry # all available telemetry drivers
-		test_ppm
 		tone_alarm
 		uavcan
 # GD drivers
 		temp_lm75
 		batt_pac17xx
-		#pca9536
-		#pwr_isl28022
-
 	MODULES
 		airspeed_selector
 		attitude_estimator_q
@@ -76,8 +66,11 @@ px4_add_board(
 		ekf2
 		esc_battery
 		events
+		flight_mode_manager
 		fw_att_control
 		fw_pos_control_l1
+		gyro_calibration
+		gyro_fft
 		land_detector
 		landing_target_estimator
 		load_mon
@@ -91,10 +84,12 @@ px4_add_board(
 		#micrortps_bridge
 		navigator
 		rc_update
-		#rover_pos_control
+		rover_pos_control
 		sensors
 		sih
 		temperature_compensation
+		uuv_att_control
+		uuv_pos_control
 		vmount
 		vtol_att_control
 		
@@ -110,6 +105,7 @@ px4_add_board(
 		hardfault_log
 		i2cdetect
 		led_control
+		mft
 		mixer
 		motor_ramp
 		motor_test
@@ -121,22 +117,27 @@ px4_add_board(
 		reboot
 		reflect
 		sd_bench
-		tests # tests and test runner
+		serial_test
+		system_time
 		top
 		topic_listener
 		tune_control
+		uorb
 		usb_connected
 		ver
 		work_queue
 	EXAMPLES
-#		bottle_drop # OBC challenge
-#		fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
-#		hello
-#		hwtest # Hardware test
+		fake_gps
+		#fake_imu
+		#fake_magnetometer
+		#fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
+		#hello
+		#hwtest # Hardware test
 		#matlab_csv_serial
-#		px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
-#		px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
-#		rover_steering_control # Rover example app
-#		uuv_example_app
-
+		#px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		#px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		#rover_steering_control # Rover example app
+		#uuv_example_app
+		#work_item
 	)
+		
